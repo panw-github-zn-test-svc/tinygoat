43e632b4ecca0560eced1e3520a52cee8f023461
Johnathan Smith <jrsmith5@waketech.edu>
2018-07-18 13:47:01 -0400

    dist first

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      }
+    ],
+  },
+  plugins: [
+    
+    new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+    //   new ExtractTextPlugin('main.css'),
+    new ProgressBarPlugin()
+  ]
+};
