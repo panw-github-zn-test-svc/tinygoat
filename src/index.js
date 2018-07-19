315d5ae6c040e2e17d061ab54e70950c5d9fd0ec
Johnathan Smith <jrsmith5@waketech.edu>
2018-07-19 15:23:31 -0400

    actual logo hover

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import style from "./main.scss";
 require('./particles')
+
+
+const normal = document.getElementById("tinygoat-label-normal")
 const funky = document.getElementById("tinygoat-label-funky")
-funky.classList.add("transparent")
-//
-document.getElementById('tinygoat-label-1').addEventListener("mouseover", event => {
-    event.target.classList.add('transparent')
+const nav = document.getElementById("nav")
+
+
+nav.addEventListener("mouseenter", event => {
+    normal.classList.add('transparent')
     funky.classList.remove("transparent")
 })
-document.getElementById('tinygoat-label-1').addEventListener("mouseout", event => {
-    event.target.classList.remove('transparent')
+
+
+nav.addEventListener("mouseleave", event => {
+    normal.classList.remove('transparent')
     funky.classList.add("transparent")
 })
