28ed50706f0862c3b8dadf4c2098b5c81315fea4
Johnathan Smith <jrsmith5@waketech.edu>
2018-07-19 15:17:16 -0400

    fancy logo hover

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import style from "./main.scss";
-require('particles.js')
+require('./particles')
+const funky = document.getElementById("tinygoat-label-funky")
+funky.classList.add("transparent")
+//
+document.getElementById('tinygoat-label-1').addEventListener("mouseover", event => {
+    event.target.classList.add('transparent')
+    funky.classList.remove("transparent")
+})
+document.getElementById('tinygoat-label-1').addEventListener("mouseout", event => {
+    event.target.classList.remove('transparent')
+    funky.classList.add("transparent")
+})
