3c4e4b222ec7c1c4c80e329caaa37592b1e9205d
Johnathan Smith <jrsmith5@waketech.edu>
2018-07-19 17:01:40 -0400

    no funky

--- src/index.js
+++ src/index.js
@@ -5,28 +5,21 @@ require('./particles')
 const normal = document.getElementById("tinygoat-label-normal")
 const funky = document.getElementById("tinygoat-label-funky")
 const nav = document.getElementById("nav")
+//
+// function isMobileDevice() {
+//     return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+// };
 
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    if (isMobileDevice()) {
-        normal.classList.add('transparent')
-        funky.classList.remove("transparent")
-    } else {
-        nav.addEventListener("mouseenter", event => {
-            normal.classList.add('transparent')
-            funky.classList.remove("transparent")
-        })
-
-
-        nav.addEventListener("mouseleave", event => {
-            normal.classList.remove('transparent')
-            funky.classList.add("transparent")
-        })
-
-    }
-});
+// nav.addEventListener("mouseenter", event => {
+//     normal.classList.add('transparent')
+//     funky.classList.remove("transparent")
+// })
+//
+//
+// nav.addEventListener("mouseleave", event => {
+//     normal.classList.remove('transparent')
+//     funky.classList.add("transparent")
+// })
+//
+//
+// document.addEventListener('DOMContentLoaded', () => {});
